df606475c2f6b7ba88d83df50c6137a43540cd72
gawe-dev <86444834+gawe-dev@users.noreply.github.com>
2024-06-29 20:26:23 -0300

    Animaciones del enemigo - musket - musket bullet fix

--- Musketeer/MusketeerBullet.gd
+++ Musketeer/MusketeerBullet.gd
@@ -1,33 +1,54 @@
 extends Area3D
 
-const RANGE :int = 20
-const DAMAGE:int = 1
-var shooted:bool = false
+
+const RANGE  : int = 20
+const DAMAGE : int = 1
+var velocity : int = 0
+var shooted : bool = false
+
 
 func _ready():
 	connect("body_entered",hit_entered)
 	
 
+
 func _process(delta):
 	if shooted:
-		global_position.x += global_basis.z.x * 5 * delta
-		global_position.y += global_basis.z.y * 5 * delta
-		global_position.z += global_basis.z.z * 5 * delta
-		
+		global_position.x += global_basis.z.x * velocity * delta
+		global_position.y += global_basis.z.y * velocity * delta
+		global_position.z += global_basis.z.z * velocity * delta
+	if (initial_position - global_position).length() > RANGE:
+		desactivar()
+
+
+func activar():
+	velocity = 15
+	visible = true
+	shooted = true
+	set_deferred("monitoring", true)
+
+func desactivar():
+	velocity = 0
+	shooted = false
+	visible = false
+	set_deferred("monitoring", false)
 
 
 func shootBullet(target_position):
 	initial()
-	visible = true
-	shooted = true
+	activar()
 	look_at(target_position, Vector3.UP, true)
 
+
+var initial_position : Vector3
 func initial():
-		global_position = $"../..".global_position
+	initial_position = $"../..".global_position
+	global_position = initial_position
+
 
 func hit_entered(body:Node3D):
 	if shooted and not body.is_in_group("Enemy"):
 		if body.has_method("GetDamage"):
 			body.GetDamage(DAMAGE)
-		shooted = false
-		visible = false
+		desactivar()
+
